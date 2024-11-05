8ad8969b48e8a6a0376c777b15d4881b0e6f00eb
kalidoscop <ranismmd@gmail.com>
2024-11-05 19:34:21 +0000

    publicité

--- src/app/pages/component/footer/footer.component.ts
+++ src/app/pages/component/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { IconsModule } from '../../../_icons/icons.module';
+import { CarouselModule } from '../carousel/carousel.module';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [IconsModule],
+  imports: [IconsModule,CarouselModule,NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
@@ -21,4 +23,40 @@ export class FooterComponent {
   email = 'contact.kondzi.com@gmail.com'
   tel = '+228 93 49 66 64'
   web = 'www.kondzi.com'
+
+  images = [
+    {
+      imageSrc: 'images/PUB 1.jpg',
+      imageAlt: 'image1',
+    },
+    {
+      imageSrc: 'images/PUB 2.jpg',
+      imageAlt: 'image2',
+    },
+  ];
+
+  selectedIndex =0
+
+  ngOnInit(): void {
+    // console.log(this.images);
+    
+    setInterval(()=>{
+      this.deroule()
+      // console.log(this.selectedIndex);
+      
+    },4000)
+    
+    // throw new Error('Method not implemented.');
+  }
+
+  deroule(){
+    // console.log(this.selectedIndex);
+    
+    if (this.selectedIndex === this.images.length-1) {
+      this.selectedIndex=0
+    }else{
+
+      this.selectedIndex+=1
+    }
+  }
 }
