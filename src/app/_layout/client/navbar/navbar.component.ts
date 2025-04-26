caf90b77af91f1c663ac115a4fb624682a06bcc2
kalidoscop <ranismmd@gmail.com>
2025-04-26 17:37:16 +0000

    mobile image 3

--- src/app/_layout/client/navbar/navbar.component.ts
+++ src/app/_layout/client/navbar/navbar.component.ts
@@ -44,23 +44,23 @@ export class NavbarComponent implements OnInit, OnDestroy {
   }
   images = [
     {
-      imageSrc: 'images/image defilante.jpg',
+      imageSrc: 'images/image phone1.jpg',
       imageAlt: 'image1',
       text: 'Plateforme réseau N°1 des acteurs professionnels de santé en Afrique',
     },
     {
-      imageSrc: 'images/01.jpg',
+      imageSrc: 'images/image phone2.jpg',
       imageAlt: 'image2',
       text: 'Une urgence vitale de santé, consultez KONDZI.COM',
     },
     {
-      imageSrc: 'images/02.jpg',
+      imageSrc: 'images/image phone3.jpg',
       imageAlt: 'image3',
       text: 'Des informations en santé fiables à portée de main sur KONDZI.COM',
     },
 
     {
-      imageSrc: 'images/03.jpg',
+      imageSrc: 'images/image phone4.jpg',
       imageAlt: 'image4',
       text: 'KONDZI.COM, des renseignements 24h/24 partout en Afrique',
     },
