12141d4b82df87e06627204c9ee1f578a0c39ef1
kalidoscop <ranismmd@gmail.com>
2025-05-19 16:46:18 +0000

    start new dashboard design

--- src/app/pages/dashboard/menu/menu-routing.module.ts
+++ src/app/pages/dashboard/menu/menu-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NosMissionComponent } from './nos-mission/nos-mission.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: [
-      { path: '', redirectTo: 'nos-mission', pathMatch: 'full' },
-      { path: 'nos-mission', component: NosMissionComponent },
-    ],
-  },
+  // {
+  //   path: '',
+  //   children: [
+  //     { path: '', redirectTo: 'nos-mission', pathMatch: 'full' },
+  //     { path: 'nos-mission', component: NosMissionComponent },
+  //   ],
+  // },
+  { path: '', component: NosMissionComponent },
 ];
 
 @NgModule({
